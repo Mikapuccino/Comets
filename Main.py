053cc4b8f56d0479a8e0cfe049c3c7950fc1e079
Nelson <nelsonfcmilheiro@gmail.com>
2022-12-16 16:57:37 +0000

    Early player movement

--- Main.py
+++ Main.py
@@ -8,16 +8,42 @@ def main():
     res = (800, 600)
     screen = pygame.display.set_mode(res)
 
+    playerImage = pygame.image.load("PlayerShip.png")
+
+    playerPos = (384,284)
+    screen.blit(playerImage, playerPos)
+
+    newPlayerX = 384
+    newPlayerY = 284
+
     exit = False
 
     while(not exit):
-
+        
         events = pygame.event.get()
         for evt in events:
             if (evt.type == pygame.QUIT):
                 exit = True
 
+        if pygame.key.get_pressed()[pygame.K_RIGHT] == True:
+            playerImage = pygame.transform.rotate(playerImage, -90)
+
+        if pygame.key.get_pressed()[pygame.K_LEFT] == True:
+            playerImage = pygame.transform.rotate(playerImage, 90)
+
+        if pygame.key.get_pressed()[pygame.K_UP] == True:
+            newPlayerY = newPlayerY - 0.2
+
+        if pygame.key.get_pressed()[pygame.K_DOWN] == True:
+            newPlayerY = newPlayerY + 0.2
+
+
+
         screen.fill((0, 0, 20))
+
+        screen.blit(playerImage, playerPos)
+
+        playerPos = (newPlayerX, newPlayerY)
 
         pygame.display.flip()
 
