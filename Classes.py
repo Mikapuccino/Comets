693aab3a73239c230573b6ab06de3f09412596f6
Nelson <nelsonfcmilheiro@gmail.com>
2022-12-22 17:07:08 +0000

    Add comments to code, reset leaderboard

--- Classes.py
+++ Classes.py
@@ -3,7 +3,15 @@ from pygame.math import Vector2
 from pygame.transform import rotozoom
 from Functions import *
 
+# This file is used to create all the classes the game will use,
+# additionally being used to run the entire game inside the classes
+
+# This variable is used to define the up angle, this being pointed directly up
+
 UP = Vector2(0, -1)
+
+# This class is the main one that is used to run the whole game
+# The main logic for the game is all contained within this class
 
 class Comets:
 
@@ -11,6 +19,8 @@ class Comets:
 
     def __init__(self):
 
+        # Defines every variable the game needs
+        
         self.initPygame()
         self.screen = pygame.display.set_mode((800, 600))
         self.clock = pygame.time.Clock()
@@ -33,6 +43,8 @@ class Comets:
         self.timeLeaderboard = 0
         self.end = True
 
+        # Creates x asteroids at the start of the game, x = 3 for now
+
         for i in range(3):
 
             while True:
@@ -43,6 +55,8 @@ class Comets:
             
             self.asteroids.append(Asteroid(pos, self.asteroids.append))
     
+    # This function detects all game objects that exist
+
     def get_GameObject(self):
 
         gameObjects = [*self.bullets, *self.asteroids]
@@ -51,6 +65,8 @@ class Comets:
             gameObjects.append(self.player)
 
         return gameObjects
+
+    # Main game loop
 
     def main(self):
 
@@ -66,9 +82,14 @@ class Comets:
                 self.gameLogic()
                 self.draw()
 
+    # This is only used to initiate pygame
+
     def initPygame(self):
 
         pygame.init()
+
+    # This function is used to display the main menu and to do
+    # the option the player selects
 
     def main_menu(self):
 
@@ -118,6 +139,9 @@ class Comets:
         pygame.display.flip()
         self.clock.tick(60)
 
+    # This funtion is used to detect the player's inputs during the game
+    # and make the appropriate action
+
     def inputLogic(self):
 
         events = pygame.event.get()
@@ -152,11 +176,29 @@ class Comets:
             if pygame.key.get_pressed()[pygame.K_UP] == True:
                 self.player.accelerate()
 
+    # This function is used to move all objects and detect collisions between objects
+    # and doing the appropriate action for each collision
+    # Also used to create more asteroids when there are no more asteroids in the game
+
     def gameLogic(self):
         
         for gameObject in self.get_GameObject():
             
                 gameObject.move(self.screen)
+
+        if self.asteroids == []:
+
+            for i in range(3):
+
+                while True:
+                    pos = random_position(self.screen)
+
+                    if (pos.distance_to(self.player.pos) > self.min_distance):
+                        break
+            
+                self.asteroids.append(Asteroid(pos, self.asteroids.append))
+
+        # If the player collides with an asteroid, the player dies and it's game over
 
         if self.player:
             for asteroid in self.asteroids:
@@ -164,6 +206,9 @@ class Comets:
                     self.player = None
                     self.phrase = "GAME OVER"
                     break
+
+        # If a bullet collides with an asteroid, it splits it if possible or it destroys it
+        # Raises the player score by 100
 
         for bullet in self.bullets[:]:
             for asteroid in self.asteroids:
@@ -174,6 +219,8 @@ class Comets:
                     self.score = self.score + 100
                     break
         
+        # If a bullet has existed for 4 seconds, the bullet disappears
+
         for bullet in self.bullets[:]:
             timePassed = pygame.time.get_ticks()
             bulletTime = bullet.timeShot
@@ -181,6 +228,10 @@ class Comets:
                 self.bullets.remove(bullet)
                 break
         
+    # This functions is used to properly display each object
+    # on the screen, with proper orientation, and to detect
+    # if the player has died, calling the gameOver function
+    
     def draw(self):
 
         self.screen.fill((0, 0, 20))
@@ -193,6 +244,10 @@ class Comets:
         
         pygame.display.flip()
         self.clock.tick(60)
+
+    # This function is used to display the game over screen and call the leaderboard
+    # screen, asking for an input from the player if they got a better score
+    # than one of the best 10 scores
 
     def gameOver(self):
         self.screen.fill((0, 0, 20))
@@ -211,6 +266,8 @@ class Comets:
 
             self.screen.fill((0, 0, 20))
             
+            # Opens leaderboard file and detects the scores in it
+
             leaderboard = open("Leaderboard.txt", "r")
 
             skip = 2
@@ -238,6 +295,8 @@ class Comets:
             score10 = score10.split(" ")
 
             leaderboard.close()
+
+            # Detects if the player got a better score than what is on the leaderboard
 
             if self.score > int(score1[1]):
                 self.editLeaderboard(3)
@@ -279,6 +338,9 @@ class Comets:
                 self.editLeaderboard(12)
                 self.score = 0
 
+            # Displays the leaderboard, with the new score if the player got a higher score
+            # than one that was already on the leaderboard
+
             leaderboard = open("Leaderboard.txt", "r")
 
             text_in_line(self.screen, leaderboard.readline(), self.font, 40, "white")
@@ -305,6 +367,9 @@ class Comets:
                 self.end = True
                 self.start = False
 
+    # This function is used to edit the leaderboard when the player
+    # got a higher score than one that is already on it
+
     def editLeaderboard(self, lineEdit):
 
         x = 0
@@ -312,6 +377,10 @@ class Comets:
         playerName = input("Input your name: ")
         leaderboard = open("Leaderboard.txt", "r")
         contents = leaderboard.readlines()
+
+        # Edits the leaderboard to put the player initials and score on the proper
+        # placement and moves every score below it, also removing the previous score in last place
+
         while (lineEdit + x) < len(contents):
 
             bottomUp = len(contents) - x - 1
@@ -329,6 +398,8 @@ class Comets:
         leaderboard.writelines(contents)
         leaderboard.close()
     
+    # This function displays the leaderboard for 6 seconds
+
     def showLeaderboard(self, ended):
 
         timePassed = pygame.time.get_ticks()
@@ -343,6 +414,9 @@ class Comets:
             self.phrase = ""
             ended = True
             return(ended)
+
+    # This function is used to restart the game everytime the player
+    # chooses start in the main menu
 
     def restart(self):
 
@@ -370,6 +444,9 @@ class Comets:
             
             self.asteroids.append(Asteroid(pos, self.asteroids.append))
 
+# This class is inherited by every other game object
+# Used to define functions and variables that all game objects use
+
 class GameObject:
 
     def __init__(self, pos, sprite, velocity):
@@ -379,19 +456,28 @@ class GameObject:
         self.radius = sprite.get_width() / 2
         self.velocity = Vector2(velocity)
 
+    # Used to draw the object on the screen
+
     def draw(self, surface):
 
         blitPos = self.pos - Vector2(self.radius)
         surface.blit(self.sprite, blitPos)
 
+    # Used to move the object on the screen and wrap around the screen
+    # if it moves beyond an edge of the screen
+    
     def move(self, surface):
 
         self.pos = wrap_position((self.pos + self.velocity), surface)
+
+    # Used to detect if there was a collision
 
     def collision(self, otherObj):
 
         distance = self.pos.distance_to(otherObj.pos)
         return distance < self.radius + otherObj.radius
+
+# This class is used for the player, used to move, accelerate, rotate and shoot bullets
 
 class Player(GameObject):
 
@@ -404,9 +490,13 @@ class Player(GameObject):
         self.direction = Vector2(UP)
         super().__init__(pos, load_sprite("PlayerShip"), Vector2(0))
 
+    # Used to change the speed at which the player moves
+
     def accelerate(self):
 
         self.velocity += self.direction * self.Acceleration
+
+    # Used to properly rotate the player and the sprite
 
     def rotate(self, clockwise=True):
         
@@ -421,6 +511,8 @@ class Player(GameObject):
 
         self.direction.rotate_ip(angle)
 
+    # Used to display the player sprite with the proper rotation
+
     def draw(self, surface):
 
         angle = self.direction.angle_to(UP)
@@ -429,12 +521,18 @@ class Player(GameObject):
         blitPos = self.pos - rotatedSurfaceSize * 0.5
         surface.blit(rotatedSurface, blitPos)
 
+    # Used to shoot bulets from the front of the player,
+    # with speed depending on the player speed
+
     def shoot(self):
 
         bulletVelocity = self.direction * self.BulletSpeed + self.velocity
         timeShot = pygame.time.get_ticks()
         bullet = Bullet(self.pos, bulletVelocity, timeShot)
         self.create_bullet_callback(bullet)
+
+# This class is used for the asteroids, used for splitting the asteroids 
+# properly depending on their size
 
 class Asteroid(GameObject):
 
@@ -446,6 +544,9 @@ class Asteroid(GameObject):
         sprite = rotozoom(load_sprite("Asteroid"), 0, scale)
         super().__init__(pos, sprite, random_velocity(1, 5))
     
+    # This function is used to split the asteroid, splitting a big asteroid
+    # into 3 medium asteroids and a medium asteroid into 5 small ones
+    
     def split(self):
         if self.size > 1:
             if self.size == 3:
@@ -456,6 +557,9 @@ class Asteroid(GameObject):
                 for i in range (5):
                     asteroid = Asteroid(self.pos, self.asteroid_callback, self.size - 1)
                     self.asteroid_callback(asteroid)
+
+# This class is used for bullets, only defining the time at which the bullet was fired
+# This is used to determine the cooldown and the lifespan of the bullet
 
 class Bullet(GameObject):
     def __init__(self, pos, velocity, timeShot):
--- Functions.py
+++ Functions.py
@@ -4,6 +4,11 @@ from pygame.math import Vector2
 from pygame import Color
 import random
 
+# This file is used to define additional functions used in the game
+
+# This function loads the proper sprite for each object,
+# also removing transparent spaces in the image
+
 def load_sprite(name, withAlpha = True):
 
     path = f"{name}.png"
@@ -17,6 +22,8 @@ def load_sprite(name, withAlpha = True):
 
         return loadedSprite.convert()
 
+# This function is used to wrap an object around the screen when it reaches an edge
+
 def wrap_position(pos, surface):
 
     x,y = pos
@@ -24,10 +31,16 @@ def wrap_position(pos, surface):
 
     return Vector2(x % w, y % h)
 
+# This function is used to determine a random position for an object
+# This is only used when creating fully new asteroids
+
 def random_position(surface):
 
     return Vector2(random.randrange(surface.get_width()), 
     random.randrange(surface.get_height()))
+
+# This function is used to determine a random position for an object
+# This is only used when creating new asteroids
 
 def random_velocity(min_velocity, max_velocity):
 
@@ -36,6 +49,8 @@ def random_velocity(min_velocity, max_velocity):
 
     return Vector2(speed, 0).rotate(angle)
 
+# This function is used to display text in the center of the screen
+
 def text(surface, text, font, color=Color("tomato")):
 
     textSurface = font.render(text, True, color)
@@ -43,6 +58,9 @@ def text(surface, text, font, color=Color("tomato")):
     rect.center = Vector2(surface.get_size()) / 2
 
     surface.blit(textSurface, rect)
+
+# This function is used to display text in the screen
+# at a specific height and in the middle of the screen's width 
 
 def text_in_line(surface, text, font, pos, color=Color("tomato")):
 
